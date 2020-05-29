23acf890f040d0629a1f9a136fd7afa76262cba7
vineethkuttan <66076509+vineethkuttan@users.noreply.github.com>
2020-05-29 13:33:54 +0530

    Update superstring.py

--- superstring.py
+++ superstring.py
@@ -1,41 +1,41 @@
 import sys 
 str=''
 def findOverlappingPair(str1,str2):
-	max = -sys.maxize-1
-	len1 = len(str1)
-	len2 = len(str2)
+    max=-sys.maxsize-1
+    len1=len(str1)
+    len2=len(str2)
     for i in range(1,min(len1,len2)):
-		if(str1[len1-i:i]==str2[0:i]):
-			if (max < i):
-				max = i
-				str = str1 + str2[i:]
-	for i in range(1,min(len1,len2)):
-		if (str1[0:i]== str2[ len2-i:i]) 
-			if (max < i):
-				max = i
-				str = str2 + str1[i:] 
-	return max 
+        if(str1[len1-i:i]==str2[0:i]):
+            if (max < i):
+                max = i
+                str = str1 + str2[i:]
+    for i in range(1,min(len1,len2)):
+	    if (str1[0:i]== str2[ len2-i:i]) :
+		    if (max < i):
+		        max = i
+		        str = str2 + str1[i:] 
+    return max 
 def findShortestSuperstring(arr,len):
-	while(len != 1):
-    	max = -sys.maxsize-1
-		resStr=''
-		for i in range(len):
-            for j in range(i+1,len): 
-				res = findOverlappingPair(arr[i], arr[j]) 
-				if (max < res):
-					max = res 
-					resStr=str 
-					l = i, r = j; 
-
-		len-=1
-		if (max == INT_MIN):
-			arr[0] += arr[len]; 
-		else: 
-			arr[l] = resStr 
-			arr[r] = arr[len]
-	return arr[0] 
+    while(len != 1):
+        max=-sys.maxsize-1
+        resStr=''
+        for i in range(len):
+            for j in range(i+1,len):
+                res = findOverlappingPair(arr[i], arr[j])
+                if (max < res):
+                    max = res
+                    resStr=str
+                    l = i, r = j
+        len-=1
+        if (max == -sys.maxsize-1):
+            arr[0] += arr[len]
+        else:
+            arr[l] = resStr
+            arr[r] = arr[len]
+    return arr[0] 
 
 li = ["catgc", "ctaagt", "gcta", "ttca", "atgcatc"]
-len=len(li)
-findShortestSuperstring(arr, len)
-print(str)
+len1=len(li)
+k=findShortestSuperstring(li, len1)
+print(k)
+print(len(k))
